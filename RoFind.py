d97274259d086343923ca2db81f2f83b1631d621
Nizzy <96216527+Niz2y@users.noreply.github.com>
2022-03-29 12:28:45 +0800

    Update RoFind.py

--- RoFind.py
+++ RoFind.py
@@ -1,9 +1,11 @@
 import json, requests
 
 def GetName(Input):
-    Info = json.loads(requests.get(f'http://verify.eryn.io/api/user/{Input}').text)['robloxUsername']
-    return Info
+    with requests.Session() as Session:
+        Info = json.loads(Session.get(f'http://verify.eryn.io/api/user/{Input}').text)['robloxUsername']
+        return Info
 
 def GetID(Input):
-    Info = json.loads(requests.get(f'http://verify.eryn.io/api/user/{Input}').text)['robloxId']
-    return Info
+    with requests.Session() as Session:
+        Info = json.loads(Session.get(f'http://verify.eryn.io/api/user/{Input}').text)['robloxId']
+        return Info
